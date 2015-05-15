7fb8a2863c9ca9d4067f3cc96a684d300eabd1a1
LI-WEI TSENG <tsengliwei@gmail.com>
2015-05-15 13:05:59 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# VCLA-Robot-Eyes
---
+++ www/js/controllers.js
@@ -0,0 +1,48 @@
+angular.module('starter.controllers', [])
+
+.controller('DashCtrl', function($scope) {})
+
+.controller('ChatsCtrl', function($scope, Chats) {
+  $scope.chats = Chats.all();
+  $scope.remove = function(chat) {
+    Chats.remove(chat);
+  }
+})
+
+.controller('ChatDetailCtrl', function($scope, $stateParams, Chats) {
+  $scope.chat = Chats.get($stateParams.chatId);
+})
+
+.controller('AccountCtrl', function($scope) {
+  $scope.settings = {
+    enableFriends: true
+  };
+})
+
+.controller('PictureCtrl', function($scope, Camera) {
+
+ $scope.getPhoto = function() {
+    console.log('Getting camera');
+    Camera.getPicture({
+      quality: 75,
+      targetWidth: 320,
+      targetHeight: 320,
+      saveToPhotoAlbum: false
+    }).then(function(imageURI) {
+      console.log(imageURI);
+      $scope.lastPhoto = imageURI;
+    }, function(err) {
+      console.err(err);
+    });
+    /*
+    navigator.camera.getPicture(function(imageURI) {
+      console.log(imageURI);
+    }, function(err) {
+    }, { 
+      quality: 50,
+      destinationType: Camera.DestinationType.DATA_URL
+    });
+    */
+  }
+
+});
